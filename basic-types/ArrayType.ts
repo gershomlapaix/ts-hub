e609bd2c8406f49da97a8bf7738d384875f3ceb7
gershomlapaix <nsengiyumv33@gmail.com>
2022-11-15 17:34:30 +0200

    array

---
+++ basic-types/ArrayType.ts
@@ -0,0 +1,14 @@
+let skills: string[];
+skills = ["TS"];
+
+console.log(skills.length);
+
+let series = [1, 2, 3];
+let doubled = series.map((num) => num * 2);
+console.log(doubled);
+
+/// storing mixed types values
+let scores: (string | number)[];
+scores = ["Programming", 6, 'SWD', 6];
+
+export {};
