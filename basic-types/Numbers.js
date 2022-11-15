1da3fecdf9b19f1d3c8bfed4bcfd8f68a6392ea9
gershomlapaix <nsengiyumv33@gmail.com>
2022-11-15 17:20:42 +0200

    numbers

---
+++ basic-types/Numbers.js
@@ -0,0 +1,12 @@
+"use strict";
+/** Numbers: All numbers in TypeScript are either floating-point
+    alues that get the number type or big integers that get the bigint type */
+exports.__esModule = true;
+var price; // floating-point
+var bagPrice = 9.95;
+var x = 100, y;
+// binary numbers
+var bin = 4;
+var anotherBin = 2;
+// big integers
+var big = 9007199254740991n;
---
+++ basic-types/Numbers.ts
@@ -0,0 +1,16 @@
+/** Numbers: All numbers in TypeScript are either floating-point 
+    alues that get the number type or big integers that get the bigint type */
+
+let price: number; // floating-point
+let bagPrice = 9.95;
+let x: number = 100,
+  y: 200;
+
+// binary numbers
+let bin = 0b100;
+let anotherBin: number = 0b010;
+
+// big integers
+let big: bigint = 9007199254740991n;
+
+export {};
