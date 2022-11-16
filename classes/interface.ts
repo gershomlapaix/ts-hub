a1a7e7d319c39025500159541ba02daed9b85530
gershomlapaix <nsengiyumv33@gmail.com>
2022-11-16 09:07:25 +0200

    interfaces: class types

--- classes/interface.ts
+++ classes/interface.ts
@@ -27,8 +27,35 @@ console.log(fullName); // Jane Doe
 /**
  * optional properties
  * readonly propeties
- * 
+ *
  */
 
-
 // function types
+interface stringFormat {
+  (str: string, isUpper: boolean): string;
+}
+
+let format: stringFormat;
+
+format = function (src: string, upper: boolean) {
+  return upper ? src.toLocaleUpperCase() : src.toLocaleLowerCase();
+};
+
+console.log(format("la", true));
+
+// Class types
+
+interface Json {
+  toJSON(): string;
+}
+
+class Person3 implements Json {
+  constructor(private firstName: string, private lastName: string) {}
+
+  toJSON(): string {
+    return JSON.stringify(this);
+  }
+}
+
+let psn = new Person3("La", "Gersh");
+console.log(psn.toJSON());
