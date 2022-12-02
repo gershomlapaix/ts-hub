6f9e685e8047d2ea304e5f7f8d8fd363a399578e
gershomlapaix <nsengiyumv33@gmail.com>
2022-12-03 01:49:43 +0200

    using currency formatter function

--- react-ts-proj/src/components/StoreItem.tsx
+++ react-ts-proj/src/components/StoreItem.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Card } from "react-bootstrap";
+import { formatCurrency } from "./../utils/formatCurrency";
 
 type StoreItemProps = {
   id: number;
@@ -19,8 +20,8 @@ export default function StoreItem({ id, name, price, imgUrl }: StoreItemProps) {
 
       <Card.Body className="d-flex flex-column">
         <Card.Title className="d-flex justify-content-between align-items-baseline mb-4">
-            <span className="fs-2">{name}</span>
-            <span className="ms-2 text-muted">${price}</span>
+          <span className="fs-2">{name}</span>
+          <span className="ms-2 text-muted">{formatCurrency(price)}</span>
         </Card.Title>
       </Card.Body>
     </Card>
