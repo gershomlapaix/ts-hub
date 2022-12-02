f71c4229a6dcf0e6914d229a505b44ad0b5b0861
gershomlapaix <nsengiyumv33@gmail.com>
2022-12-03 01:47:12 +0200

    adding currency formater utility function

--- react-ts-proj/src/pages/Store.tsx
+++ react-ts-proj/src/pages/Store.tsx
@@ -1,9 +1,15 @@
-export default function Store() {
-    
+import { Row, Col } from "react-bootstrap";
+import StoreItem from "../components/StoreItem";
+import storeItems from "../data/items.json";
 
-    return (
-        <>
-            Store
-        </>
-    )
+export default function Store() {
+  return (
+    <Row md={2} xs={1} lg={3} className="g-3">
+      {storeItems.map((item) => (
+        <Col key={item.id}>
+            <StoreItem {...item}/>
+        </Col>
+      ))}
+    </Row>
+  );
 }
