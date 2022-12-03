3f05e4cc62b9aaba68514b69a3fa2b4a8ab3ee3f
gershomlapaix <nsengiyumv33@gmail.com>
2022-12-03 02:20:53 +0200

    adding cart handler functions

--- react-ts-proj/src/context/ShoppingCartContext.tsx
+++ react-ts-proj/src/context/ShoppingCartContext.tsx
@@ -4,6 +4,13 @@ type ShoppingCartProviderProps = {
   children: ReactNode;
 };
 
+type ShoppingCartContext = {
+  getItemQuantity: (id: number) => number;
+  increaseCartQuantity: (id: number) => void;
+  decreaseCartQuantity: (id: number) => void;
+  removeFromCart: (id: number) => void;
+};
+
 const ShoppingCartContext = createContext({});
 
 export function useShoppingCart() {
